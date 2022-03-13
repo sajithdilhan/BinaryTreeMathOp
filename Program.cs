f48d837e8123e9f33e4b47387e8278649317373b
Sajith Dilhan <sajithdilhan@gmail.com>
2022-03-13 12:06:50 +0800

    Basic traversing

--- Program.cs
+++ Program.cs
@@ -6,7 +6,53 @@ namespace BinaryTreeMathOp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node root = new Node { Value="-"};
+
+            root.Left = new Node { Value="*" };
+            root.Left.Left = new Node { Value="/" };
+            root.Left.Right = new Node { Value="-" };
+            root.Left.Left.Left = new Node { Value="15"};
+            root.Left.Left.Right = new Node { Value = "-" };
+            root.Left.Left.Right.Left = new Node { Value = "7" };
+            root.Left.Left.Right.Right = new Node { Value = "+" };
+            root.Left.Left.Right.Right.Left = new Node { Value = "1" };
+            root.Left.Left.Right.Right.Right = new Node { Value = "1" };
+            root.Left.Right.Right = new Node { Value="3" };
+
+            root.Right = new Node { Value = "+" };
+            root.Right.Left = new Node { Value="2" };
+            root.Right.Right = new Node { Value = "+" };
+            root.Right.Right.Right = new Node { Value = "1" };
+            root.Right.Right.Left = new Node { Value = "1" };
+
+            var Result = CalculateSubtree(root);
+        }
+
+        public static bool CheckIsALeafNode(Node node)
+        {
+            if (node != null && node.Left == null && node.Right == null)
+            return true;
+            return false;
+        }
+
+        public static double CalculateSubtree(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (CheckIsALeafNode(root))
+            {
+                return Double.Parse(root.Value);
+            }
+            else
+            {
+                double left= CalculateSubtree(root.Left);
+                double right= CalculateSubtree(root.Right);
+
+                return left + right; //ToDo: Need to Calculate the real values 
+            }
         }
     }
 
@@ -15,10 +61,6 @@ namespace BinaryTreeMathOp
         public string Value { get; set; }
         public Node Left { get; set; }
         public Node Right { get; set; }
-    }
-
-    public class Tree
-    {
 
     }
 }
