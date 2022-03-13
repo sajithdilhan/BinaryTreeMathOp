be2080ed5a18abf8164226f8293ccf50d414c6a5
Sajith Dilhan <sajithdilhan@gmail.com>
2022-03-13 12:13:47 +0800

    Calculations completed

--- Program.cs
+++ Program.cs
@@ -6,21 +6,21 @@ namespace BinaryTreeMathOp
     {
         static void Main(string[] args)
         {
-            Node root = new Node { Value="-"};
+            Node root = new Node { Value = "-" };
 
-            root.Left = new Node { Value="*" };
-            root.Left.Left = new Node { Value="/" };
-            root.Left.Right = new Node { Value="-" };
-            root.Left.Left.Left = new Node { Value="15"};
+            root.Left = new Node { Value = "*" };
+            root.Left.Left = new Node { Value = "/" };
+            root.Left.Right = new Node { Value = "-" };
+            root.Left.Left.Left = new Node { Value = "15" };
             root.Left.Left.Right = new Node { Value = "-" };
             root.Left.Left.Right.Left = new Node { Value = "7" };
             root.Left.Left.Right.Right = new Node { Value = "+" };
             root.Left.Left.Right.Right.Left = new Node { Value = "1" };
             root.Left.Left.Right.Right.Right = new Node { Value = "1" };
-            root.Left.Right.Right = new Node { Value="3" };
+            root.Left.Right.Right = new Node { Value = "3" };
 
             root.Right = new Node { Value = "+" };
-            root.Right.Left = new Node { Value="2" };
+            root.Right.Left = new Node { Value = "2" };
             root.Right.Right = new Node { Value = "+" };
             root.Right.Right.Right = new Node { Value = "1" };
             root.Right.Right.Left = new Node { Value = "1" };
@@ -31,7 +31,7 @@ namespace BinaryTreeMathOp
         public static bool CheckIsALeafNode(Node node)
         {
             if (node != null && node.Left == null && node.Right == null)
-            return true;
+                return true;
             return false;
         }
 
@@ -48,11 +48,26 @@ namespace BinaryTreeMathOp
             }
             else
             {
-                double left= CalculateSubtree(root.Left);
-                double right= CalculateSubtree(root.Right);
+                double left = CalculateSubtree(root.Left);
+                double right = CalculateSubtree(root.Right);
 
-                return left + right; //ToDo: Need to Calculate the real values 
+                return CalculateLeaves(root, left, right); 
             }
+        }
+
+        public static double CalculateLeaves(Node root, double leftValue, double rightValue)
+        {
+            string operatorChar = root.Value;
+            if (operatorChar == "+")
+                return leftValue + rightValue;
+            else if (operatorChar == "-")
+                return leftValue - rightValue;
+            else if (operatorChar == "*")
+                return leftValue * rightValue;
+            else if (operatorChar == "/")
+                return leftValue / rightValue;
+            else
+                return 0;
         }
     }
 
