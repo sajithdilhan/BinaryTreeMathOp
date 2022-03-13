06ffe858d00277dc16ad60999455d4def82a6dd3
Sajith Dilhan <sajithdilhan@gmail.com>
2022-03-13 10:05:27 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinaryTreeMathOp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Node
+    {
+        public string Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+
+    public class Tree
+    {
+
+    }
+}
